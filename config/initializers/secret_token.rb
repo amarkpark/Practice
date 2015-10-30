a954510e7ef33a10d24ae2c169bf145d40afc269
amarkpark <amarkpark@gmail.com>
2015-10-30 19:08:32 +0000

    Initial commit for Practice app for git push pull and branch quiz etc. ~AMP

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Practice::Application.config.secret_key_base = 'fd58d1eb5d017ceffa15c56cd447d30172319d824dc8fb43ce5e0e74446a16c58c0db59a375cca7bcdd1bf791604f6d2f8059d22f5db405a44e44d19768a1aae'
